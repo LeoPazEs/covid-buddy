2602a39954d1401d7f618335a9f8690eeca1cd86
Leonardo Paz <leopestevam@hotmail.com>
2022-06-17 14:59:50 -0300

    leonardo: testes de cartões corrigidos
    Signed-off-by: Leonardo Paz <leopestevam@hotmail.com>

--- test/CartaoTest.java
+++ test/CartaoTest.java
@@ -9,10 +9,10 @@ public class CartaoTest {
         Paciente paciente = new Paciente("529.982.247-25", "12345", "Leonardo Paz Estevam",  LocalDate.of(2001, 4, 5), "(82)99919-2696", "Masculino"); 
         new Visa(paciente, "4242 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1)));
 
-        Throwable numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "424 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.parse("2022-12")));
+        Throwable numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "424 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1))));
         Assertions.assertEquals( "Número inválido.", numeroInvalido.getMessage()); 
 
-        numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "4244 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.parse("2022-12")));
+        numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "4244 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1))));
         Assertions.assertEquals( "Número inválido.", numeroInvalido.getMessage());
     }
 
@@ -21,10 +21,22 @@ public class CartaoTest {
         Paciente paciente = new Paciente("529.982.247-25", "12345", "Leonardo Paz Estevam",  LocalDate.of(2001, 4, 5), "(82)99919-2696", "Masculino"); 
         new MasterCard(paciente, "5555 5555 5555 4444", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1)));
 
-        Throwable numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new MasterCard(paciente, "424 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.parse("2022-12")));
+        Throwable numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new MasterCard(paciente, "424 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1))));
         Assertions.assertEquals( "Número inválido.", numeroInvalido.getMessage()); 
 
-        numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new MasterCard(paciente, "5556 5555 5555 4444", 342, "Leonardo Paz Estevam", YearMonth.parse("2022-12")));
+        numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new MasterCard(paciente, "5556 5555 5555 4444", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1))));
         Assertions.assertEquals( "Número inválido.", numeroInvalido.getMessage());
+    } 
+
+    @Test 
+    public void criandoCartaoComValidadeInvalida(){
+        Paciente paciente = new Paciente("529.982.247-25", "12345", "Leonardo Paz Estevam",  LocalDate.of(2001, 4, 5), "(82)99919-2696", "Masculino"); 
+        new Visa(paciente, "4242 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().plusMonths(1)));
+
+        Throwable numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "4242 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now())));
+        Assertions.assertEquals( "Validade inválida.", numeroInvalido.getMessage()); 
+
+        numeroInvalido = Assertions.assertThrows(IllegalArgumentException.class, () -> new Visa(paciente, "4242 4242 4242 4242", 342, "Leonardo Paz Estevam", YearMonth.from(LocalDate.now().minusMonths(1))));
+        Assertions.assertEquals( "Validade inválida.", numeroInvalido.getMessage());
     }
 }
